dba200cfc29b666cf07fd5531c1bec4f4f628e1a
Arvydas <azegaspa@gmail.com>
2025-05-06 19:37:31 +0300

    fetches init

---
+++ ADF.Fetches/cvbankas/config.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+# CVBankas
+CVBANKAS_KEYWORDS = ["django", "vadovas"]
+CVBANKAS_IGNORE_WORDS_IN_JOB_TITLE = [
+    "Senior",
+    "Stat",
+    "Konstrukt",
+    "aplinkos",
+    "elektr",
+    "ūki",
+    "Šilum",
+    "Pamain",
+    "metal",
+    "pardavim",
+    "personal",
+    "rinkod",
+    "auto",
+    "restor",
+    "pardavim",
+    "pirkim",
+]
+CVBANKAS_IGNORE_WORDS_IN_JOB_COMPANY = ["EASTCON", "pastat"]
