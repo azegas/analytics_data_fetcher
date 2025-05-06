6002d2b68f5c04e0c214ac61cb8b1e6996cb0205
Arvydas <azegaspa@gmail.com>
2025-05-06 22:07:58 +0300

    job utils page + some TODO's

---
+++ ADF.Fetches/cvbankas/job_utils.py
@@ -0,0 +1,68 @@
+import requests
+from extractors import (
+    extract_articles,
+    extract_id,
+    extract_link,
+    extract_title,
+    extract_company,
+    extract_salary,
+    extract_when_posted,
+    extract_city,
+    extract_category_from_detail_page,
+)
+import json
+import os
+from datetime import datetime
+from log_config import logger
+
+
+def fetch_cvbankas_jobs(pagesToFetch):
+    logger.info("Fetching CVBankas jobs...")
+    jobs = []
+
+    for page in range(1, pagesToFetch):
+        url = f"https://en.cvbankas.lt/?page={page}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            articles = extract_articles(response)
+
+            for job in articles:
+                job_data = {
+                    "job_id": extract_id(job),
+                    "link": extract_link(job),
+                    "title": extract_title(job),
+                    "company": extract_company(job),
+                    "salary": extract_salary(job),
+                    "posted": extract_when_posted(job),
+                    "city": extract_city(job),
+                    "category": extract_category_from_detail_page(
+                        extract_link(job)
+                    ),
+                }
+                jobs.append(job_data)
+
+        except Exception as e:
+            logger.error(f"Error fetching data for page {page}: {e}")
+
+    logger.info(f"Fetched {len(jobs)} job listings")
+    return jobs
+
+
+def save_cvbankas_jobs(jobs):
+    data_to_save = {
+        "fetch_date": datetime.now().strftime("%Y-%m-%d %H:%M"),
+        "jobs": jobs,
+    }
+
+    file_path = os.path.join(os.getenv("BASE_DIR"), "data/cvbankas_ads.json")
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    try:
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(data_to_save, f, ensure_ascii=False, indent=4)
+        logger.info(f"Data saved to {file_path}")
+    except IOError as e:
+        logger.error(f"Failed to save data: {e}")
--- ADF.Fetches/cvbankas/main.py
+++ ADF.Fetches/cvbankas/main.py
@@ -1,90 +1,23 @@
-import requests
-from extractors import (
-    extract_articles,
-    extract_id,
-    extract_link,
-    extract_title,
-    extract_company,
-    extract_salary,
-    extract_when_posted,
-    extract_city,
-    extract_category_from_detail_page,
-)
-import json
-import os
-import sys
-from datetime import datetime
+from job_utils import fetch_cvbankas_jobs, save_cvbankas_jobs
 from dotenv import load_dotenv
-
-# Add the parent directory to sys.path (for log_config)
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from log_config import logger
-
-pagesToFetch = 2  # how many pages to fetch?
-
-load_dotenv()
-
-# start going from the last page, take only those whose expiration is less than 1 day (hours)
-# get rekvizitai info
-
-
-def fetch_cvbankas_jobs():
-    logger.info("Fetching CVBankas jobs...")
-    jobs = []
-
-    for page in range(1, pagesToFetch):
-
-        url = f"https://en.cvbankas.lt/?page={page}"
-
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-
-            articles = extract_articles(response)
-
-            for job in articles:
-
-                job_data = {
-                    "job_id": extract_id(job),
-                    "link": extract_link(job),
-                    "title": extract_title(job),
-                    "company": extract_company(job),
-                    "salary": extract_salary(job),
-                    "posted": extract_when_posted(job),
-                    "city": extract_city(job),
-                    "category": extract_category_from_detail_page(
-                        extract_link(job)
-                    ),
-                }
-                jobs.append(job_data)
-
-        except Exception as e:
-            logger.error(f"Error fetching data for page {page}: {e}")
-
-    logger.info(f"Fetched {len(jobs)} job listings")
-    return jobs
-
-
-def save_cvbankas_jobs(jobs):
-    data_to_save = {
-        "fetch_date": datetime.now().strftime("%Y-%m-%d %H:%M"),
-        "jobs": jobs,
-    }
-
-    file_path = os.path.join(os.getenv("BASE_DIR"), "data/cvbankas_ads.json")
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-    try:
-        with open(file_path, "w", encoding="utf-8") as f:
-            json.dump(data_to_save, f, ensure_ascii=False, indent=4)
-        logger.info(f"Data saved to {file_path}")
-    except IOError as e:
-        logger.error(f"Failed to save data: {e}")
 
 
 def main():
-    jobs = fetch_cvbankas_jobs()
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # TODO find the total amount of pages
+    # TODO start going from the last page, open only the ones that expire today (hours written instead of days)
+    # TODO fetch all towns, if there are more
+    # TODO fetch the views
+    # TODO fetch the applications
+    # TODO fetch rekvizitai info
+    # TODO think about how to fetch and save requirements
+
+    # Fetch jobs from CVBankas
+    jobs = fetch_cvbankas_jobs(2)
+
+    # Save the fetched jobs to a file
     save_cvbankas_jobs(jobs)
 
 
